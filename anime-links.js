ffd4d8548c590b422a4be3af613a52945f6baca1
TRAP <68010652+trap010101@users.noreply.github.com>
2026-09-06 14:22:56 +0900

    Bust cache for streaming selector fix

--- anime-links.js
+++ anime-links.js
@@ -106,6 +106,6 @@
     .then(() => loadScript('/data/streaming-jp-20260906.js?v=20260906-region1'))
     .then(() => loadScript('/data/streaming-us-20260906.js?v=20260906-region1'))
     .then(() => loadScript('/streaming-region-ui.js?v=20260906-region1'))
-    .then(() => loadScript('/streaming-region-compact.js?v=20260906-region3'))
+    .then(() => loadScript('/streaming-region-compact.js?v=20260906-region4'))
     .catch(error => console.warn('Regional streaming UI could not be loaded.', error));
 })();
