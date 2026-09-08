9b6210e61d14ee52ab5ff324e7e3517681dda1b7
TRAP <68010652+trap010101@users.noreply.github.com>
2026-09-08 17:08:21 +0900

    Gate Google auth until provider setup

--- auth-config.js
+++ auth-config.js
@@ -3,5 +3,6 @@
 // Do not place service_role/secret keys or Google client secrets in this file.
 window.NEWANIME_AUTH_CONFIG = Object.freeze({
   supabaseUrl: 'https://ojpgkxxojwkoczvwpcyy.supabase.co',
-  supabaseAnonKey: 'sb_publishable_j9OgtJdmg4Q8B05ToOlnfQ_2OzPkzt2'
+  supabaseAnonKey: 'sb_publishable_j9OgtJdmg4Q8B05ToOlnfQ_2OzPkzt2',
+  googleEnabled: false
 });
