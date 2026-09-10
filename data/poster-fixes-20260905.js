ca2801219fafd4c4ffc3a017219fec451581806e
TRAP <68010652+trap010101@users.noreply.github.com>
2026-09-10 17:16:45 +0900

    Restore high-resolution poster sources

--- data/poster-fixes-20260905.js
+++ data/poster-fixes-20260905.js
@@ -53,8 +53,8 @@
     "unrewarded-villager-a": { src: "assets/posters/unrewarded-villager-a-v3.webp", updatedAt: "2026-09-07" },
     "glasses-sometimes-yankee-kun": { src: "assets/posters/glasses-sometimes-yankee-kun-v2.webp", updatedAt: "2026-09-07" },
     "we-are-aliens": { src: "assets/posters/we-are-aliens.webp", updatedAt: "2026-09-07" },
-    "a-certain-dark-sides-shared-living": { src: "assets/posters/a-certain-dark-sides-shared-living-user-20260910-v3.webp", updatedAt: "2026-09-10" },
-    "nia-liston-the-merciless-maiden": { src: "assets/posters/nia-liston-the-merciless-maiden-user-20260910-v3.webp", updatedAt: "2026-09-10" },
+    "a-certain-dark-sides-shared-living": { src: "https://ogre.natalie.mu/media/news/comic/2026/0222/toaruitem_teaser.jpg?imdensity=1&impolicy=hq&imwidth=1460", updatedAt: "2026-09-10" },
+    "nia-liston-the-merciless-maiden": { src: "https://kyoranreijo-pr.com/wp/wp-content/uploads/2026/07/%E3%82%AD%E3%83%BC%E3%83%93%E3%82%B8%E3%83%A5%E3%82%A2%E3%83%AB-scaled.jpg", updatedAt: "2026-09-10" },
     "sss-class-revival-hunter": { src: "assets/posters/sss-class-revival-hunter.webp", updatedAt: "2026-09-10" }
   };
 
